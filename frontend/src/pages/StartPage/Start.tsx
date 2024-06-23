0fbffe2e52fb920513f8ae74a9b845c6c7c4d5d6
medvalna <v.m.30.03.20@gmail.com>
2024-06-23 19:30:55 +0300

    chore: shadcn config fixes

--- frontend/src/pages/StartPage/Start.tsx
+++ frontend/src/pages/StartPage/Start.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@/components/ui/button"
+
 export const Start = () => {
     return <div className="flex items-center justify-center flex-col">
       <h1 className="text-center bg-red-200">TourFood</h1>
-      {/* <Button /> */}
-          </div>
+      <Button>Start</Button>
+        </div>
 }
