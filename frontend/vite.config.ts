fdc6ba631fe63e0ea657f38d068baea925809b95
medvalna <v.m.30.03.20@gmail.com>
2024-06-23 19:30:55 +0300

    chore: shadcn config fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import path from 'path'
-
-// https://vitejs.dev/config/
-
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+ 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  // resolve: {
-  //   alias: {
-  //     "@": path.resolve(__dirname, "./src"),
-  //   },
-  // },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
