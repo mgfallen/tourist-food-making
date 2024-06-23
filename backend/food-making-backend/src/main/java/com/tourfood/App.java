861412d21058cbd698c8e1d4d140623b9ee97a17
sunvis0r <94429755+sunvis0r@users.noreply.github.com>
2024-06-24 01:41:09 +0500

    Переменные сделал константами

--- backend/food-making-backend/src/main/java/com/tourfood/App.java
+++ backend/food-making-backend/src/main/java/com/tourfood/App.java
@@ -12,39 +12,39 @@ import org.jsoup.select.Elements;
 
 public class App {
 	public static void main(String[] args) {
-		String websiteDomain = "https://yarcheplus.ru";
-		String website = websiteDomain + "/catalog/ovoschi-i-frukty-187"; // TODO Получать каждую веб-страницу из https://yarcheplus.ru/
-		String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"; // Если убрать, то yarcheplus.ru будет выдавать «Извините, ваш браузер не поддерживается»
-		String CSSselectorWorkzone = "#app-content > div > div:last-of-type > div:nth-of-type(2) > div:first-of-type > div:last-of-type > div:last-of-type > div:last-of-type";
-		String CSSselectorCategories = "#app-content > div > div:last-of-type > div:first-of-type > div > a:has(> picture)";
-		String CSSselectorPagination = "> div:last-of-type > div:last-of-type > a:has(svg)";
-		String CSSSelectorProduct = "> div:first-of-type > div > div"; // Почему нужно с «>»? Без него ничего не работает
-		String CSSselectorProductTitle = "> div:nth-of-type(2) > div:nth-of-type(2) > div:first-of-type"; // Его содержимое типа: «Томаты Черри Делтари, 250&nbsp;г», «Чеснок молодой», «Чеснок 3 шт», «Лимоны поштучно, 0,1 - 0,3 кг», «Лайм 1 шт.», «Капуста белокочанная Свежий урожай поштучно, 1,2 - 4,5 кг», «Голубика», «Манго желтое, поштучно, 0,3 - 0,8 кг» (желтое и поштучно нужно будет убирать)
-		String CSSselectorProductQuantity = "> div:nth-of-type(2) > div:nth-of-type(1) > div:nth-of-type(2)"; // Содержимое здесь — это граммовка. Формат: «250 г» или «1 кг» или «1 шт.»
-		String CSSselectorProductPrice = "> div:nth-of-type(2) > div:nth-of-type(1) > div:nth-of-type(1) > div:first-of-type"; // TODO ВНИМАНИЕ: захватывает только самую дешёвую цену (которая по скидке). В будущем захватывать обе цены для предоставления выбора пользователю. Формат: «169,99 ₽»
-		String categoriesExclusions = "https://yarcheplus.ru/catalog/newest-732 https://yarcheplus.ru/catalog/bestseller-731 https://yarcheplus.ru/catalog/detskoe-pitanie-i-gigiena-224 https://yarcheplus.ru/catalog/igrushki-216 https://yarcheplus.ru/catalog/dlya-doma-223 https://yarcheplus.ru/catalog/krasota-i-zdorovye-220 https://yarcheplus.ru/catalog/zootovary-219 https://yarcheplus.ru/catalog/kolgotki-i-noski-173 https://yarcheplus.ru/catalog/podarochnye-pakety-830 https://yarcheplus.ru/catalog/melochi-u-kassy-762"; // TODO В будущем сделать белый список категорий в виде диапазона
+		final String WEBSITE_DOMAIN = "https://yarcheplus.ru";
+		final String WEBSITE = WEBSITE_DOMAIN + "/catalog/ovoschi-i-frukty-187"; // TODO Получать каждую веб-страницу из https://yarcheplus.ru/
+		final String WEBSITE_USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"; // Если убрать, то yarcheplus.ru будет выдавать «Извините, ваш браузер не поддерживается»
+		final String CSSSELECTOR_WORKZONE = "#app-content > div > div:last-of-type > div:nth-of-type(2) > div:first-of-type > div:last-of-type > div:last-of-type > div:last-of-type";
+		final String CSSSELECTOR_CATEGORIES = "#app-content > div > div:last-of-type > div:first-of-type > div > a:has(> picture)";
+		final String CSSSELECTOR_PAGINATION = "> div:last-of-type > div:last-of-type > a:has(svg)";
+		final String CSSSELECTOR_PRODUCT = "> div:first-of-type > div > div"; // Почему нужно с «>»? Без него ничего не работает
+		final String CSSSELECTOR_PRODUCT_TITLE = "> div:nth-of-type(2) > div:nth-of-type(2) > div:first-of-type"; // Его содержимое типа: «Томаты Черри Делтари, 250&nbsp;г», «Чеснок молодой», «Чеснок 3 шт», «Лимоны поштучно, 0,1 - 0,3 кг», «Лайм 1 шт.», «Капуста белокочанная Свежий урожай поштучно, 1,2 - 4,5 кг», «Голубика», «Манго желтое, поштучно, 0,3 - 0,8 кг» (желтое и поштучно нужно будет убирать)
+		final String CSSSELECTOR_PRODUCT_QUANTITY = "> div:nth-of-type(2) > div:nth-of-type(1) > div:nth-of-type(2)"; // Содержимое здесь — это граммовка. Формат: «250 г» или «1 кг» или «1 шт.»
+		final String CSSSELECTOR_PRODUCT_PRICE = "> div:nth-of-type(2) > div:nth-of-type(1) > div:nth-of-type(1) > div:first-of-type"; // TODO ВНИМАНИЕ: захватывает только самую дешёвую цену (которая по скидке). В будущем захватывать обе цены для предоставления выбора пользователю. Формат: «169,99 ₽»
+		final String CATEGORIES_EXCLUSIONS = "https://yarcheplus.ru/catalog/newest-732 https://yarcheplus.ru/catalog/bestseller-731 https://yarcheplus.ru/catalog/detskoe-pitanie-i-gigiena-224 https://yarcheplus.ru/catalog/igrushki-216 https://yarcheplus.ru/catalog/dlya-doma-223 https://yarcheplus.ru/catalog/krasota-i-zdorovye-220 https://yarcheplus.ru/catalog/zootovary-219 https://yarcheplus.ru/catalog/kolgotki-i-noski-173 https://yarcheplus.ru/catalog/podarochnye-pakety-830 https://yarcheplus.ru/catalog/melochi-u-kassy-762"; // TODO В будущем сделать белый список категорий в виде диапазона
 
 		Document doc = null;
-		String currWebpage = new String(website);
+		String currWebpage = new String(WEBSITE);
 		String nextPageLink = null;
 
 		List<String> categoriesExclusionsList = new ArrayList<>();
-		String[] items = categoriesExclusions.split(" ");
+		String[] items = CATEGORIES_EXCLUSIONS.split(" ");
 		categoriesExclusionsList.addAll(Arrays.asList(items));
 
 		// TODO Убрать шаблонный код (для парсинга первых продуктов устанавливается ДВА последовательных подключения)
 		try {
-			doc = Jsoup.connect(currWebpage).userAgent(userAgent).get();
+			doc = Jsoup.connect(currWebpage).userAgent(WEBSITE_USERAGENT).get();
 		} catch (IOException e) {
 			System.err.println("Не удалось связаться с веб-страницей «" + currWebpage + "».");
 			e.printStackTrace();
 			System.exit(1);
 		}
-		Elements categories = doc.select(CSSselectorCategories);
+		Elements categories = doc.select(CSSSELECTOR_CATEGORIES);
 		List<String> categoriesList = new ArrayList<>();
 		for (Element category : categories) {
 			String categoryLink = category.attr("href");
-			categoriesList.add(websiteDomain + categoryLink);
+			categoriesList.add(WEBSITE_DOMAIN + categoryLink);
 		}
 		
 		// Исключение из одного списка другой
@@ -55,18 +55,18 @@ public class App {
 			currWebpage = categoryLink;
 			do {
 				try {
-					doc = Jsoup.connect(currWebpage).userAgent(userAgent).get();
+					doc = Jsoup.connect(currWebpage).userAgent(WEBSITE_USERAGENT).get();
 				} catch (IOException e) {
 					System.err.println("Не удалось связаться с веб-страницей «" + currWebpage + "».");
 					e.printStackTrace();
 					System.exit(1);
 				}
-				Elements products = doc.select(CSSselectorWorkzone).select(CSSSelectorProduct);
+				Elements products = doc.select(CSSSELECTOR_WORKZONE).select(CSSSELECTOR_PRODUCT);
 
 				for (Element everyProduct : products) {
-					String title = everyProduct.select(CSSselectorProductTitle).first().text();
-					String quantity = everyProduct.select(CSSselectorProductQuantity).first().text();
-					String price = everyProduct.select(CSSselectorProductPrice).first().text(); // TODO Цена парсится за килограмм, а не за штуку
+					String title = everyProduct.select(CSSSELECTOR_PRODUCT_TITLE).first().text();
+					String quantity = everyProduct.select(CSSSELECTOR_PRODUCT_QUANTITY).first().text();
+					String price = everyProduct.select(CSSSELECTOR_PRODUCT_PRICE).first().text(); // TODO Цена парсится за килограмм, а не за штуку
 					String[] titleArray = extractMeasureRange(title); // Примерная граммовка одной единицы товара (допустим, вес арбуза)
 					String name = titleArray[0];
 					String measureRange = titleArray[1];
@@ -81,11 +81,11 @@ public class App {
 					// TODO Обработать то, что каждая из этих переменных может быть ПОЧЕМУ-ТО пустой
 				}
 				try {
-					nextPageLink = doc.select(CSSselectorWorkzone).select(CSSselectorPagination).first().attr("href");
+					nextPageLink = doc.select(CSSSELECTOR_WORKZONE).select(CSSSELECTOR_PAGINATION).first().attr("href");
 				} catch (NullPointerException e) {
 					break;
 				}
-				currWebpage = websiteDomain + new String(nextPageLink);
+				currWebpage = WEBSITE_DOMAIN + new String(nextPageLink);
 			} while (nextPageLink != null);
 			// TODO В будущем реализовать пагинацию с помощью нажатия кнопки (`<button>`)
 		}
