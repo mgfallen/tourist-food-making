b2b3a873fec200b1e12926c065b9918cd392c78d
mgfallen <minegoodfeeling@gmail.com>
2024-06-25 19:26:05 +0300

    added based entities

--- backend/food-making-backend/src/main/java/org/example/entities/Order.java
+++ backend/food-making-backend/src/main/java/org/example/entities/Order.java
@@ -1,2 +1,16 @@
-package org.example.entities;public class Order {
+package org.example.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "order_id")
+    private Long orderId;
+
 }
--- backend/food-making-backend/src/main/java/org/example/entities/User.java
+++ backend/food-making-backend/src/main/java/org/example/entities/User.java
@@ -1,4 +1,20 @@
 package org.example.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 100, nullable = false)
+    private String token;
 }
