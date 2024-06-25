0f75f85bd51aaf5566929952b3bfd31c0313a51a
medvalna <v.m.30.03.20@gmail.com>
2024-06-26 00:11:33 +0300

    feat: added navigation

--- frontend/src/pages/MenuPage/MenuPage.tsx
+++ frontend/src/pages/MenuPage/MenuPage.tsx
@@ -1,12 +1,12 @@
 import { Button } from '@/components/ui/button'
 import { useNavigate } from 'react-router-dom'
 
-export const FilterPage = () => {
+export const MenuPage = () => {
   const navigate = useNavigate()
   return (
     <div className="flex items-center justify-center flex-col">
-      <h1 className="text-center bg-red-200">FilterPage</h1>
-      <Button onClick={() => navigate('/menu')}>Breakfast</Button>
+      <h1 className="text-center bg-red-200">MenuPage</h1>
+      <Button onClick={() => navigate('/recipe')}>Soup</Button>
     </div>
   )
 }
