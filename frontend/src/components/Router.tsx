d9cd7c745f47c6788899bdcbecd6b49d6f4c129a
medvalna <v.m.30.03.20@gmail.com>
2024-06-26 00:11:33 +0300

    feat: added navigation

--- frontend/src/components/Router.tsx
+++ frontend/src/components/Router.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import App from './App'
-import { Start } from '../pages/StartPage/Start'
-import { Filter } from '@/pages/FilterPage/Filter'
+import { StartPage } from '../pages/StartPage/Start'
+import { FilterPage } from '@/pages/FilterPage/Filter'
+import { MenuPage } from '@/pages/MenuPage/MenuPage'
+import { RecipePage } from '@/pages/ResipePage/ResipePage'
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<App />} path="/"> 
-        <Route element={<Start />} path="/" />
-        <Route element={<Filter />} path="/filter" />
+        <Route element={<App />} path="/">
+          <Route element={<StartPage />} path="/" />
+          <Route element={<FilterPage />} path="/filter" />
+          <Route element={<MenuPage />} path="/menu" />
+          <Route element={<RecipePage />} path="/recipe" />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
--- frontend/src/pages/ResipePage/ResipePage.tsx
+++ frontend/src/pages/ResipePage/ResipePage.tsx
@@ -1,12 +1,12 @@
 import { Button } from '@/components/ui/button'
 import { useNavigate } from 'react-router-dom'
 
-export const FilterPage = () => {
+export const RecipePage = () => {
   const navigate = useNavigate()
   return (
     <div className="flex items-center justify-center flex-col">
-      <h1 className="text-center bg-red-200">FilterPage</h1>
-      <Button onClick={() => navigate('/menu')}>Breakfast</Button>
+      <h1 className="text-center bg-red-200">RecipePage</h1>
+      <Button onClick={() => navigate('/')}>Start</Button>
     </div>
   )
 }
