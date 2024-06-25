6e0c0321c7f24891c6619f7ca3a9a1f6d27081be
mgfallen <minegoodfeeling@gmail.com>
2024-06-25 19:26:36 +0300

    added product controller

--- backend/food-making-backend/src/main/java/org/example/controllers/ProductController.java
+++ backend/food-making-backend/src/main/java/org/example/controllers/ProductController.java
@@ -1,7 +1,27 @@
 package org.example.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.example.entities.Product;
+import org.example.services.ProductDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class ProductController {
+
+    @Autowired
+    private final ProductDataService productDataService;
+
+    public ProductController(ProductDataService productDataService) {
+        this.productDataService = productDataService;
+    }
+
+    @GetMapping(value = "/products")
+    public ResponseEntity<List<Product>> getProducts() {
+        List<Product> productList = productDataService.getAllProducts();
+        return ResponseEntity.ok(productList);
+    }
 }
