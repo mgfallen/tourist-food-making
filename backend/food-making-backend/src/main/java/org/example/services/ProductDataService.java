c3944aedde4b26a972dad462199efc086dc1518f
mgfallen <minegoodfeeling@gmail.com>
2024-06-25 19:29:43 +0300

    added product service layer

--- backend/food-making-backend/src/main/java/org/example/services/ProductDataService.java
+++ backend/food-making-backend/src/main/java/org/example/services/ProductDataService.java
@@ -1,2 +1,24 @@
-package org.example.services;public class ProductDataService {
+package org.example.services;
+
+import jakarta.transaction.Transactional;
+import org.example.entities.Product;
+import org.example.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductDataService {
+
+    private ProductRepository productRepository;
+
+    @Transactional
+    public Product getProductById(Long id) {
+        return productRepository.getById(id);
+    }
+
+    @Transactional
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
 }
