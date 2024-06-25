1a8f8f7c9511df8a66ab1fc998730267d6924342
mgfallen <minegoodfeeling@gmail.com>
2024-06-25 19:26:05 +0300

    added based entities

--- backend/food-making-backend/src/main/java/org/example/entities/Portion.java
+++ backend/food-making-backend/src/main/java/org/example/entities/Portion.java
@@ -1,4 +1,25 @@
 package org.example.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "portion")
 public class Portion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long portion_id;
+
+    @OneToMany(mappedBy = "recipe")
+    private Long recipeId;
+
+    @OneToMany (mappedBy = "product")
+    private List<Product> products;
+
+    @Column(name = "summary_price")
+    private Long summaryPrice;
+
 }
--- backend/food-making-backend/src/main/java/org/example/entities/Product.java
+++ backend/food-making-backend/src/main/java/org/example/entities/Product.java
@@ -1,7 +1,43 @@
 package org.example.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
+@Table(name = "product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer proteins;
+
+    @Column(nullable = false)
+    private Integer fats;
+
+    @Column(nullable = false)
+    private Integer carbohydrates;
+
+    @Column(length = 150)
+    private String link;
+
+    @Column(nullable = false)
+    private Integer kilocalories;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(name="pack_weight")
+    private Integer packWeight;
+
+    private Boolean perishable;
+
+    @Column(length = 50)
+    private String category;
 }
