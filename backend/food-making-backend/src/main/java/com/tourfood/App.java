71a62adee88032b55198d18a60b476722f4ce514
sunvis0r <94429755+sunvis0r@users.noreply.github.com>
2024-06-26 21:22:56 +0500

    Теперь программа принимает параметр «recipes» в командной строке
    Добавил в игнор папку bin

--- backend/food-making-backend/src/main/java/com/tourfood/App.java
+++ backend/food-making-backend/src/main/java/com/tourfood/App.java
@@ -32,176 +32,181 @@ public class App {
 	}
 
 	public static void main(String[] args) {
-		final String WEBSITE_DOMAIN = "https://yarcheplus.ru";
-		final String WEBSITE = WEBSITE_DOMAIN + "/catalog/ovoschi-i-frukty-187"; // TODO Получать каждую веб-страницу из https://yarcheplus.ru/
-		final String WEBSITE_USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"; // Если убрать, то yarcheplus.ru будет выдавать «Извините, ваш браузер не поддерживается»
-		final String CSSSELECTOR_WORKZONE_PRODUCTS = "#app-content > div > div:last-of-type > div:nth-of-type(2) > div:first-of-type > div:last-of-type > div:last-of-type > div:last-of-type";
-		final String CSSSELECTOR_WORKZONE_PRODUCTDIALOG = "main#app > div:not(#app-content) > div > div[role=\"dialog\"] div:has(> div > h1)";
-		final String CSSSELECTOR_CATEGORIES = "#app-content > div > div:last-of-type > div:first-of-type > div > a:has(> picture)";
-		final String CSSSELECTOR_PAGINATION = "> div:last-of-type > div:last-of-type > a:has(svg)";
-		final String CSSSELECTOR_PRODUCT = "> div:first-of-type > div > div";
-		final String CSSSELECTOR_PRODUCT_LINK = "> a"; // Почему нужно с «>»? Без него ничего не работает
-		final String CSSSELECTOR_PRODUCT_TITLE = "> div:nth-of-type(2) > div:nth-of-type(2) > div:first-of-type"; // Его содержимое типа: «Томаты Черри Делтари, 250&nbsp;г», «Чеснок молодой», «Чеснок 3 шт», «Лимоны поштучно, 0,1 - 0,3 кг», «Лайм 1 шт.», «Капуста белокочанная Свежий урожай поштучно, 1,2 - 4,5 кг», «Голубика», «Манго желтое, поштучно, 0,3 - 0,8 кг» (желтое и поштучно нужно будет убирать)
-		final String CSSSELECTOR_PRODUCT_QUANTITY = "> div:nth-of-type(2) > div:nth-of-type(1) > div:nth-of-type(2)"; // Содержимое здесь — это граммовка. Формат: «250 г» или «1 кг» или «1 шт.»
-		final String CSSSELECTOR_PRODUCT_PRICE = "> div:nth-of-type(2) > div:nth-of-type(1) > div:nth-of-type(1) > div:first-of-type"; // TODO ВНИМАНИЕ: захватывает только самую дешёвую цену (которая по скидке). В будущем захватывать обе цены для предоставления выбора пользователю. Формат: «169,99 ₽»
-		final String CSSSELECTOR_CATEGORY_NAME = "a > div";
-		final String CATEGORIES_EXCLUSIONS = "https://yarcheplus.ru/catalog/newest-732 https://yarcheplus.ru/catalog/bestseller-731 https://yarcheplus.ru/catalog/detskoe-pitanie-i-gigiena-224 https://yarcheplus.ru/catalog/igrushki-216 https://yarcheplus.ru/catalog/dlya-doma-223 https://yarcheplus.ru/catalog/krasota-i-zdorovye-220 https://yarcheplus.ru/catalog/zootovary-219 https://yarcheplus.ru/catalog/kolgotki-i-noski-173 https://yarcheplus.ru/catalog/podarochnye-pakety-830 https://yarcheplus.ru/catalog/melochi-u-kassy-762"; // TODO В будущем сделать белый список категорий в виде диапазона
-		final String JSON_FILEPATH = "output.json";
+		if (args.length != 0 && args[0].equals("recipes")) {
+			System.out.println("Парсинг рецептов...");
+		} else {
+			System.out.println("Парсинг продуктов...");
+			final String WEBSITE_DOMAIN = "https://yarcheplus.ru";
+			final String WEBSITE = WEBSITE_DOMAIN + "/catalog/ovoschi-i-frukty-187"; // TODO Получать каждую веб-страницу из https://yarcheplus.ru/
+			final String WEBSITE_USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"; // Если убрать, то yarcheplus.ru будет выдавать «Извините, ваш браузер не поддерживается»
+			final String CSSSELECTOR_WORKZONE_PRODUCTS = "#app-content > div > div:last-of-type > div:nth-of-type(2) > div:first-of-type > div:last-of-type > div:last-of-type > div:last-of-type";
+			final String CSSSELECTOR_WORKZONE_PRODUCTDIALOG = "main#app > div:not(#app-content) > div > div[role=\"dialog\"] div:has(> div > h1)";
+			final String CSSSELECTOR_CATEGORIES = "#app-content > div > div:last-of-type > div:first-of-type > div > a:has(> picture)";
+			final String CSSSELECTOR_PAGINATION = "> div:last-of-type > div:last-of-type > a:has(svg)";
+			final String CSSSELECTOR_PRODUCT = "> div:first-of-type > div > div";
+			final String CSSSELECTOR_PRODUCT_LINK = "> a"; // Почему нужно с «>»? Без него ничего не работает
+			final String CSSSELECTOR_PRODUCT_TITLE = "> div:nth-of-type(2) > div:nth-of-type(2) > div:first-of-type"; // Его содержимое типа: «Томаты Черри Делтари, 250&nbsp;г», «Чеснок молодой», «Чеснок 3 шт», «Лимоны поштучно, 0,1 - 0,3 кг», «Лайм 1 шт.», «Капуста белокочанная Свежий урожай поштучно, 1,2 - 4,5 кг», «Голубика», «Манго желтое, поштучно, 0,3 - 0,8 кг» (желтое и поштучно нужно будет убирать)
+			final String CSSSELECTOR_PRODUCT_QUANTITY = "> div:nth-of-type(2) > div:nth-of-type(1) > div:nth-of-type(2)"; // Содержимое здесь — это граммовка. Формат: «250 г» или «1 кг» или «1 шт.»
+			final String CSSSELECTOR_PRODUCT_PRICE = "> div:nth-of-type(2) > div:nth-of-type(1) > div:nth-of-type(1) > div:first-of-type"; // TODO ВНИМАНИЕ: захватывает только самую дешёвую цену (которая по скидке). В будущем захватывать обе цены для предоставления выбора пользователю. Формат: «169,99 ₽»
+			final String CSSSELECTOR_CATEGORY_NAME = "a > div";
+			final String CATEGORIES_EXCLUSIONS = "https://yarcheplus.ru/catalog/newest-732 https://yarcheplus.ru/catalog/bestseller-731 https://yarcheplus.ru/catalog/detskoe-pitanie-i-gigiena-224 https://yarcheplus.ru/catalog/igrushki-216 https://yarcheplus.ru/catalog/dlya-doma-223 https://yarcheplus.ru/catalog/krasota-i-zdorovye-220 https://yarcheplus.ru/catalog/zootovary-219 https://yarcheplus.ru/catalog/kolgotki-i-noski-173 https://yarcheplus.ru/catalog/podarochnye-pakety-830 https://yarcheplus.ru/catalog/melochi-u-kassy-762"; // TODO В будущем сделать белый список категорий в виде диапазона
+			final String JSON_FILEPATH = "output.json";
 
-		Document doc = null;
-		String currWebpage = new String(WEBSITE);
-		String nextPageLink = null;
+			Document doc = null;
+			String currWebpage = new String(WEBSITE);
+			String nextPageLink = null;
 
-		List<String> categoriesExclusionsList = new ArrayList<>();
-		String[] items = CATEGORIES_EXCLUSIONS.split(" ");
-		categoriesExclusionsList.addAll(Arrays.asList(items));
+			List<String> categoriesExclusionsList = new ArrayList<>();
+			String[] items = CATEGORIES_EXCLUSIONS.split(" ");
+			categoriesExclusionsList.addAll(Arrays.asList(items));
 
-		// TODO Убрать шаблонный код (для парсинга первых продуктов устанавливается ДВА последовательных подключения)
-		try {
-			doc = Jsoup.connect(currWebpage).userAgent(WEBSITE_USERAGENT).get();
-		} catch (IOException e) {
-			System.err.println("Не удалось связаться с веб-страницей «" + currWebpage + "».");
-			e.printStackTrace();
-			System.exit(1);
-		}
-		System.out.println("Подключение к веб-странице «" + currWebpage + "» успешно установлено.");
-		Elements categories = doc.select(CSSSELECTOR_CATEGORIES);
-		List<Category> categoriesList = new ArrayList<>();
-		for (Element category : categories) {
-			String categoryLink = WEBSITE_DOMAIN + category.attr("href");
-			String categoryName = category.select(CSSSELECTOR_CATEGORY_NAME).text();
-			categoriesList.add(new Category(categoryLink, categoryName));
-		}
+			// TODO Убрать шаблонный код (для парсинга первых продуктов устанавливается ДВА последовательных подключения)
+			try {
+				doc = Jsoup.connect(currWebpage).userAgent(WEBSITE_USERAGENT).get();
+			} catch (IOException e) {
+				System.err.println("Не удалось связаться с веб-страницей «" + currWebpage + "».");
+				e.printStackTrace();
+				System.exit(1);
+			}
+			System.out.println("Подключение к веб-странице «" + currWebpage + "» успешно установлено.");
+			Elements categories = doc.select(CSSSELECTOR_CATEGORIES);
+			List<Category> categoriesList = new ArrayList<>();
+			for (Element category : categories) {
+				String categoryLink = WEBSITE_DOMAIN + category.attr("href");
+				String categoryName = category.select(CSSSELECTOR_CATEGORY_NAME).text();
+				categoriesList.add(new Category(categoryLink, categoryName));
+			}
 
-		// Исключение из одного списка другой
-		categoriesList.removeIf(category -> categoriesExclusionsList.contains(category.link));
-		// TODO В будущем оптимизировать (мб можно и без буферного списка)
+			// Исключение из одного списка другой
+			categoriesList.removeIf(category -> categoriesExclusionsList.contains(category.link));
+			// TODO В будущем оптимизировать (мб можно и без буферного списка)
 
-		ObjectMapper objectMapper = new ObjectMapper();
-		JsonFactory jsonFactory = new JsonFactory(objectMapper);
-		File file = new File(JSON_FILEPATH);
+			ObjectMapper objectMapper = new ObjectMapper();
+			JsonFactory jsonFactory = new JsonFactory(objectMapper);
+			File file = new File(JSON_FILEPATH);
 
-		try (FileWriter fileWriter = new FileWriter(file); JsonGenerator jsonGenerator = jsonFactory.createGenerator(fileWriter)) {
-			// Начинаем массив
-			jsonGenerator.writeStartArray();
-			System.out.println("Путь к JSON-файлу: «" + JSON_FILEPATH + "». Завершающее `]` появится только после окончания сбора данных с сайта.");
-			for (Category category : categoriesList) {
-				currWebpage = category.link;
-				do {
-					try {
-						doc = Jsoup.connect(currWebpage).userAgent(WEBSITE_USERAGENT).get();
-					} catch (IOException e) {
-						System.err.println("Не удалось связаться с веб-страницей «" + currWebpage + "».");
-						e.printStackTrace();
-						System.exit(1);
-					}
-					Elements products = doc.select(CSSSELECTOR_WORKZONE_PRODUCTS).select(CSSSELECTOR_PRODUCT);
-
-					for (Element everyProduct : products) {
-						String link = WEBSITE_DOMAIN + everyProduct.select(CSSSELECTOR_PRODUCT_LINK).first().attr("href");
-						String title = everyProduct.select(CSSSELECTOR_PRODUCT_TITLE).first().text();
-						String quantity = everyProduct.select(CSSSELECTOR_PRODUCT_QUANTITY).first().text();
-						String price = everyProduct.select(CSSSELECTOR_PRODUCT_PRICE).first().text(); // TODO Цена парсится за килограмм, а не за штуку
-						String[] titleArray = extractMeasureRange(title); // Примерная граммовка одной единицы товара (допустим, вес арбуза)
-						String name = titleArray[0];
-						String measureRange = titleArray[1];
-
-						Document docProductDialog = null;
+			try (FileWriter fileWriter = new FileWriter(file); JsonGenerator jsonGenerator = jsonFactory.createGenerator(fileWriter)) {
+				// Начинаем массив
+				jsonGenerator.writeStartArray();
+				System.out.println("Путь к JSON-файлу: «" + JSON_FILEPATH + "». Завершающее `]` появится только после окончания сбора данных с сайта.");
+				for (Category category : categoriesList) {
+					currWebpage = category.link;
+					do {
 						try {
-							docProductDialog = Jsoup.connect(link).userAgent(WEBSITE_USERAGENT).get();
+							doc = Jsoup.connect(currWebpage).userAgent(WEBSITE_USERAGENT).get();
 						} catch (IOException e) {
-							System.err.println("Не удалось связаться с веб-страницей «" + link + "».");
+							System.err.println("Не удалось связаться с веб-страницей «" + currWebpage + "».");
 							e.printStackTrace();
 							System.exit(1);
 						}
-						Elements productDialog = docProductDialog.select(CSSSELECTOR_WORKZONE_PRODUCTDIALOG + "> div");
-						byte i = 0;
-						Element productFoodEnergy = null;
-						for (Element productPropertyDiv : productDialog) {
-							Element foo = productPropertyDiv.select("a[href$='/reviews']").first();
-							// if (foo == null) {
-							// 	foo = productPropertyDiv.select("> div > div > div > div").first(); // .first().text().equalsIgnoreCase("Нет оценок")
-							// }
-							i++;
-							if (foo != null) {
-								productFoodEnergy = productDialog.get(i-2); // -1 т.к. отсчёт с нуля и -1 т.к. нужно взять более ранний элемент
+						Elements products = doc.select(CSSSELECTOR_WORKZONE_PRODUCTS).select(CSSSELECTOR_PRODUCT);
+
+						for (Element everyProduct : products) {
+							String link = WEBSITE_DOMAIN + everyProduct.select(CSSSELECTOR_PRODUCT_LINK).first().attr("href");
+							String title = everyProduct.select(CSSSELECTOR_PRODUCT_TITLE).first().text();
+							String quantity = everyProduct.select(CSSSELECTOR_PRODUCT_QUANTITY).first().text();
+							String price = everyProduct.select(CSSSELECTOR_PRODUCT_PRICE).first().text(); // TODO Цена парсится за килограмм, а не за штуку
+							String[] titleArray = extractMeasureRange(title); // Примерная граммовка одной единицы товара (допустим, вес арбуза)
+							String name = titleArray[0];
+							String measureRange = titleArray[1];
+
+							Document docProductDialog = null;
+							try {
+								docProductDialog = Jsoup.connect(link).userAgent(WEBSITE_USERAGENT).get();
+							} catch (IOException e) {
+								System.err.println("Не удалось связаться с веб-страницей «" + link + "».");
+								e.printStackTrace();
+								System.exit(1);
 							}
-						}
+							Elements productDialog = docProductDialog.select(CSSSELECTOR_WORKZONE_PRODUCTDIALOG + "> div");
+							byte i = 0;
+							Element productFoodEnergy = null;
+							for (Element productPropertyDiv : productDialog) {
+								Element foo = productPropertyDiv.select("a[href$='/reviews']").first();
+								// if (foo == null) {
+								// 	foo = productPropertyDiv.select("> div > div > div > div").first(); // .first().text().equalsIgnoreCase("Нет оценок")
+								// }
+								i++;
+								if (foo != null) {
+									productFoodEnergy = productDialog.get(i-2); // -1 т.к. отсчёт с нуля и -1 т.к. нужно взять более ранний элемент
+								}
+							}
 
-						String proteins = null;
-						String fats = null;
-						String carbohydrates = null;
-						String cal = null;
+							String proteins = null;
+							String fats = null;
+							String carbohydrates = null;
+							String cal = null;
+							try {
+								proteins = searchDivs(productFoodEnergy, "Белки");
+							} catch (NullPointerException e) {
+								proteins = "-";
+							}
+							try {
+								fats = searchDivs(productFoodEnergy, "Жиры");
+							} catch (NullPointerException e) {
+								fats = "-";
+							}
+							try {
+								carbohydrates = searchDivs(productFoodEnergy, "Углеводы");
+							} catch (NullPointerException e) {
+								carbohydrates = "-";
+							}
+							try {
+								cal = searchDivs(productFoodEnergy, "ккал");
+							} catch (NullPointerException e) {
+								carbohydrates = "-";
+							}
+
+							if (price.endsWith("₽")) {
+								price = price.substring(0, price.length() - 1).trim();
+							}
+							if (price.endsWith(" ")) {
+								price = price.substring(0, price.length() - 1).trim();
+							}
+							price = price.replaceAll(",", ".");
+
+							Matcher matcher = Pattern.compile("^(.*?),\\\\s*([0-9]+).*$").matcher(title);
+							if (matcher.find()) {
+								title = matcher.group(1);
+							}
+
+							System.out.println(category.name + "; " + name + "; " + measureRange + "; " + quantity + "; " + price + "; " + link + "; " + proteins + "; " + fats + "; " + carbohydrates + "; " + cal);
+
+							jsonGenerator.writeStartObject();
+
+							jsonGenerator.writeStringField("category", category.name);
+							jsonGenerator.writeStringField("name", name);
+							jsonGenerator.writeStringField("measureRange", measureRange);
+							jsonGenerator.writeStringField("quantity", quantity);
+							jsonGenerator.writeStringField("price", price);
+							jsonGenerator.writeStringField("link", link);
+							jsonGenerator.writeStringField("proteins", proteins);
+							jsonGenerator.writeStringField("fats", fats);
+							jsonGenerator.writeStringField("carbohydrates", carbohydrates);
+							jsonGenerator.writeStringField("calories", cal);
+
+							jsonGenerator.writeEndObject();
+							jsonGenerator.flush();
+
+							// TODO Обработать то, что каждая из этих переменных может быть ПОЧЕМУ-ТО пустой
+						}
 						try {
-							proteins = searchDivs(productFoodEnergy, "Белки");
+							nextPageLink = doc.select(CSSSELECTOR_WORKZONE_PRODUCTS).select(CSSSELECTOR_PAGINATION).first().attr("href");
 						} catch (NullPointerException e) {
-							proteins = "-";
+							break;
 						}
-						try {
-							fats = searchDivs(productFoodEnergy, "Жиры");
-						} catch (NullPointerException e) {
-							fats = "-";
-						}
-						try {
-							carbohydrates = searchDivs(productFoodEnergy, "Углеводы");
-						} catch (NullPointerException e) {
-							carbohydrates = "-";
-						}
-						try {
-							cal = searchDivs(productFoodEnergy, "ккал");
-						} catch (NullPointerException e) {
-							carbohydrates = "-";
-						}
+						currWebpage = WEBSITE_DOMAIN + new String(nextPageLink);
+					} while (nextPageLink != null);
+					// TODO В будущем реализовать пагинацию с помощью нажатия кнопки (`<button>`)
+				}
+				// Завершаем массив
+				jsonGenerator.writeEndArray();
 
-						if (price.endsWith("₽")) {
-							price = price.substring(0, price.length() - 1).trim();
-						}
-						if (price.endsWith(" ")) {
-							price = price.substring(0, price.length() - 1).trim();
-						}
-						price = price.replaceAll(",", ".");
-
-						Matcher matcher = Pattern.compile("^(.*?),\\\\s*([0-9]+).*$").matcher(title);
-						if (matcher.find()) {
-							title = matcher.group(1);
-						}
-
-						System.out.println(category.name + "; " + name + "; " + measureRange + "; " + quantity + "; " + price + "; " + link + "; " + proteins + "; " + fats + "; " + carbohydrates + "; " + cal);
-
-						jsonGenerator.writeStartObject();
-
-						jsonGenerator.writeStringField("category", category.name);
-						jsonGenerator.writeStringField("name", name);
-						jsonGenerator.writeStringField("measureRange", measureRange);
-						jsonGenerator.writeStringField("quantity", quantity);
-						jsonGenerator.writeStringField("price", price);
-						jsonGenerator.writeStringField("link", link);
-						jsonGenerator.writeStringField("proteins", proteins);
-						jsonGenerator.writeStringField("fats", fats);
-						jsonGenerator.writeStringField("carbohydrates", carbohydrates);
-						jsonGenerator.writeStringField("calories", cal);
-
-						jsonGenerator.writeEndObject();
-						jsonGenerator.flush();
-
-						// TODO Обработать то, что каждая из этих переменных может быть ПОЧЕМУ-ТО пустой
-					}
-					try {
-						nextPageLink = doc.select(CSSSELECTOR_WORKZONE_PRODUCTS).select(CSSSELECTOR_PAGINATION).first().attr("href");
-					} catch (NullPointerException e) {
-						break;
-					}
-					currWebpage = WEBSITE_DOMAIN + new String(nextPageLink);
-				} while (nextPageLink != null);
-				// TODO В будущем реализовать пагинацию с помощью нажатия кнопки (`<button>`)
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			// Завершаем массив
-			jsonGenerator.writeEndArray();
-
-		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("JSON-файл успешно создан!");
 		}
-		System.out.println("JSON-файл успешно создан!");
 	}
 
 	/**
@@ -227,7 +232,7 @@ public class App {
 
 		return measureRange;
 	}
-	
+
 	/**
 	 * <p>Рекурсивный поиск необходимых БЖУ и калорий</p>
 	 * @param element
