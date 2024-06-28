fcf351427a5e1ddd849310b500825db649879b5a
YuriewAlex <yuriew.alex2018@yandex.ru>
2024-06-29 02:54:52 +0300

    Added return of order_id

---
+++ backend/food-making-backend/src/main/java/org/example/DTO/OrderResponseDTO.java
@@ -0,0 +1,13 @@
+package org.example.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class OrderResponseDTO {
+    private Long orderID;
+    private List<RecommendationDTO> recommendations;
+}
