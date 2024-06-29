77c227921e264d3e5da9a15816476fc271f6fdbd
YuriewAlex <yuriew.alex2018@yandex.ru>
2024-06-29 04:16:11 +0300

    Fix calculating amounts

--- backend/food-making-backend/src/main/java/org/example/support/ProductsList.java
+++ backend/food-making-backend/src/main/java/org/example/support/ProductsList.java
@@ -10,4 +10,19 @@ public class ProductsList implements Serializable {
     private Long productId;
     private Double amount;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ProductsList that = (ProductsList) o;
+
+        return productId == that.productId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Math.toIntExact(productId);
+    }
+
 }
